fcd3ae6a917956ac28749d571127d3f7a5d65fbd
p1yu5h <piyush.satija@gmail.com>
2020-10-04 20:24:24 +0530

    Updates

---
+++ precache-manifest.d4fba009f20ddf3f4b915f88f65f6b46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3118e10884850639b0ad5151379df60c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c83a7c08851bd46ded75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d18a4fc8918031b020d",
+    "url": "/static/js/2.ce91a20c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce91a20c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83a7c08851bd46ded75",
+    "url": "/static/js/main.f14332c8.chunk.js"
+  },
+  {
+    "revision": "d0960ee3e0226c4ffa52",
+    "url": "/static/js/runtime-main.37d1c058.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
